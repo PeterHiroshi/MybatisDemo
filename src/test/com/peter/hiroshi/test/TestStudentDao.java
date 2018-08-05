ebf00f86360ae88db5596f1481735cac39605da3
PeterHiroshi <peter8icestone@gmail.com>
2018-08-05 21:57:20 +0800

    Add an assertion on TestStudentDao.java

--- src/test/com/peter/hiroshi/test/TestStudentDao.java
+++ src/test/com/peter/hiroshi/test/TestStudentDao.java
@@ -4,6 +4,7 @@ import com.peter.hiroshi.MybatisDemo.dao.StudentDao;
 import com.peter.hiroshi.MybatisDemo.dao.StudentDaoImpl;
 import com.peter.hiroshi.MybatisDemo.entity.Student;
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class TestStudentDao {
     public void findAllTest() {
         StudentDao studentDao = new StudentDaoImpl();
         List<Student> students = studentDao.findAll();
+        Assert.assertEquals(2, students.size());
         logger.debug(students);
     }
 
